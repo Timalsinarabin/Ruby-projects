87501054eb602ce0281f16bd1f12f7486aa6dc72
Timalsinarabin <timalsinarabin11@gmail.com>
2026-02-12 13:34:32 +0545

    feat: add number guessing game

---
+++ numberGuess.rb
@@ -0,0 +1,22 @@
+num = rand(100);
+guess = nil;
+
+while num!=guess
+    print("Enter a number(1 to 100):");
+    guess = gets.chomp.to_i;
+    if(guess > 100 || guess <1)
+        puts"Please input a valid guess";
+    else
+        if guess < num
+            puts"guess higher";
+        elsif guess > num
+            puts"guess lower";
+        else
+            puts"Your guess is correct";
+        end
+    end
+end
+
+
+
+
