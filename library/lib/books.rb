c8e5604a03da76485280aaa79fefb3fb7e06d2e2
Timalsinarabin <timalsinarabin11@gmail.com>
2026-02-20 10:50:30 +0000

    feat: Implemented sqlite3

--- library/lib/books.rb
+++ library/lib/books.rb
@@ -1,29 +1,25 @@
-require 'json'
+require 'sqlite3'
+
 # Books class to manage the library's book collection
 class Books
-  Filename = 'library/lib/books.json'.freeze
-  def add_book(title, author)
-    books = books_read
-    books << { 'title' => title, 'author' => author }
-    File.write(Filename, JSON.pretty_generate(books))
+  def initialize
+    @db = SQLite3::Database.new "lib/book.db"
+    @db.execute <<-SQL
+      CREATE TABLE IF NOT EXISTS bookinfo(
+      id INTEGER PRIMARY KEY,
+      book_name VARCHAR(50),
+      author VARCHAR(50)
+    )
+    SQL
+    
   end
-
-  def books_read
-    if File.exist?(Filename)
-      JSON.parse(File.read(Filename))
-    else
-      []
-    end
+  def add_book(title, author)
+    @db.execute "insert into bookinfo (book_name,author) values(?,?)",[title,author]
   end
 
   def list_books
-    books = books_read
-    if books.empty?
-      puts 'There are no books in the library.'
-    else
-      books.each do |book|
-        puts "Title: #{book['title']}, Author: #{book['author']}"
-      end
+    @db.execute "select id, book_name, author from bookinfo" do |row|
+      puts "#{row[0]}. Book Title: #{row[1]}\n   Author: #{row[2]}"
     end
   end
 end
--- library/lib/stud.rb
+++ library/lib/stud.rb
@@ -1,64 +1,45 @@
-require 'json'
+require 'sqlite3'
+
 # Student class to manage students and their book loans
 class Stud
-  Filename = 'library/lib/books.json'.freeze
-  StudFilename = 'library/lib/stud.json'.freeze
-  Loanfile = 'library/lib/loans.json'.freeze
-
-  def add_stud(s_id)
-    stud = stud_read
-    stud << { 's_id' => s_id }
-    File.write(StudFilename, JSON.pretty_generate(stud))
+  def initialize
+    @books = []
+    @db = SQLite3::Database.new "lib/book.db"
+    @db.execute <<-SQL
+      CREATE TABLE IF NOT EXISTS std_info(
+      id INTEGER PRIMARY KEY,
+      student_name VARCHAR(50)
+      )
+    SQL
+    @db.execute <<-SQL
+      CREATE TABLE IF NOT EXISTS loan_info(
+      id INTEGER PRIMARY KEY,
+      book_name VARCHAR(50),
+      s_id INTEGER,
+      FOREIGN KEY (s_id) REFERENCES std_info(id)
+    )
+    SQL
   end
-
-  def stud_read
-    if File.exist?(StudFilename)
-      JSON.parse(File.read(StudFilename))
-    else
-      []
-    end
-  end
-
-  def books_read
-    if File.exist?(Filename)
-      JSON.parse(File.read(Filename))
-    else
-      []
-    end
-  end
-
-  def loan_stud
-    if File.exist?(Loanfile) && !File.zero?(Loanfile)
-      JSON.parse(File.read(Loanfile))
-    else
-      []
-    end
+  def add_stud(s_id,std_name)
+    @db.execute "insert into std_info (id,student_name) values(?,?)",[s_id,std_name]
   end
 
   def list_loan
-    loans = loan_stud
-    if loans.empty?
-      puts 'There are no loans yet.'
-    else
-      loans.each_with_index do |loan, index|
-        puts "#{index + 1}. Student ID: #{loan['s_id']}, Book: #{loan['book']}"
-      end
+    puts "Student_id | Book Name"
+    @db.execute "select s_id, book_name from loan_info" do |row|  
+      puts "#{row[0]}\t   | #{row[1]}"
     end
   end
 
   def loan_book(s_id, book)
-    if stud_read.none? { |student| student['s_id'] == s_id }
-      puts "Student with ID #{s_id} does not exist."
-      return
+    @db.execute "select book_name from bookinfo" do |row|
+      @books.push(row[0])
     end
-    booklist = books_read
-    if booklist.none? { |b| b['title'] == book }
-      puts "Book titled '#{book}' does not exist in the library."
-      return
+    if @books.include?(book)
+      @db.execute "insert into loan_info (s_id,book_name) values(?,?)",[s_id,book]
+    else
+      puts"Book not found"
     end
-    puts "#{s_id} has loaned the book: #{book}"
-    loans = loan_stud
-    loans << { 's_id' => s_id, 'book' => book }
-    File.write(Loanfile, JSON.pretty_generate(loans))
   end
 end
+
